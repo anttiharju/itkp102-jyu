bb5ec9fb5b9bed9fd0ddc44951b268ec0665ddbe
anttiharju <antti@harju.io>
2019-10-04 11:29:34 +0000

    Korjattu kun drawline jättää nurkasta pikselin piirtämättä Piirretään tyhjät ruudut tumman harmaalla mukaan Loin setup aliohjelmia että pysyy begin siistinä olin vahingossa käyttänyt koodissa nimetystä table niin vaihdoin sen arrayksi (tiedä onko vieläkään oikein) nyt on static, dynamic ja draw arrayt joissa liikuttelen palikoita     dynamic sisältää tippuvan palikkamuodostelman     static ne palikat jotka on jo törmännyt     draw on ainoastaan sitä varten että piirtyy kuva jossa kummatkin mukana     oli ongelmallista pitää kaikki erillään kun ei saanut kai laittaa suoraan array = array koska silloin menee vain muistireferencillä palikkamuodostelmat pysähtyvät törmätessään muihin palikoihin tai pohjaan (dynamicArray -> staticArray)
    git-svn-id: https://svn.cc.jyu.fi/srv/svn/ohj1-pelit/2019s/anvemaha/trunk@38383 a045da7b-9728-470d-ae4b-0a4eae343f79

--- Tetris/Tetris/Tetris.cs
+++ Tetris/Tetris/Tetris.cs
@@ -7,51 +7,117 @@ using Jypeli.Widgets;
 
 public class Tetris : PhysicsGame
 {
-    int size = 50;
-    int dSize = 22;
+    public int size = 50;
+    public int dSize = 22;
 
-    int[,] playArea = new int[10, 24];
+    public int[,] drawArray = new int[10, 24];
+    public int[,] staticArray;
+    public int[,] dynamicArray;
 
     //TODO: Jaetaan pelialue kolmeen eri taulukkoon:
-    //dynamicTable = täällä on se yksi tippuva muodostelma ja sitä voi kieritellä tässä
-    //staticTable = täällä ne jämähtäneet palikat ja poistetaan täydet rivit
-    //drawTable = se mikä lähetetään tuonne paint hommelliin että ruudulla näkyy kaikki ohjen
+    //dynamicArray = täällä on se yksi tippuva muodostelma ja sitä voi kieritellä tässä
+    //staticArray = täällä ne jämähtäneet palikat ja poistetaan täydet rivit
+    //drawArray = se mikä lähetetään tuonne paint hommelliin että ruudulla näkyy kaikki ohjen
     //              tähän vain siis yhdistetty nuo kaksi muuta taulukkoa
 
     public override void Begin()
     {
+        SetupArrays();
+        SetupLevel();
+        SetupUpdateLoop();
 
-        for (int x = 0; x < playArea.GetLength(0); x++)
+        dynamicArray[0, 23] = 2;
+        dynamicArray[1, 23] = 2;
+        dynamicArray[2, 23] = 2;
+        dynamicArray[3, 23] = 2;
+        dynamicArray[4, 23] = 2;
+        dynamicArray[5, 23] = 2;
+        dynamicArray[6, 23] = 2;
+        dynamicArray[7, 23] = 2;
+        dynamicArray[8, 23] = 2;
+        dynamicArray[9, 23] = 2;
+
+        staticArray[5, 21] = 1;
+
+        drawArray = CombineArrays(staticArray, dynamicArray);
+
+        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
+        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+    }
+
+
+    /// <summary>
+    /// Yhdistää kaksi taulukkoa uuteen taulukkoon
+    /// </summary>
+    /// <param name="a">taulukko 1</param>
+    /// <param name="b">taulukko 2</param>
+    /// <returns>taulukko 1 ja 2 yhdistelmän</returns>
+    public static int[,] CombineArrays(int[,] a, int[,] b)
+    {
+        int[,] c = new int[a.GetLength(0), a.GetLength(1)];
+        for (int x = 0; x < a.GetLength(0); x++)
         {
-            for (int y = 0; y < playArea.GetLength(1); y++)
+            for (int y = 0; y < a.GetLength(1); y++)
             {
-                playArea[x, y] = 0;
+                if (a[x, y] != 0)
+                {
+                    c[x, y] = a[x, y];
+                }
+                if (b[x, y] != 0)
+                {
+                    c[x, y] = b[x, y];
+                }
             }
         }
 
-        playArea[9, 23] = 1;
-        playArea[8, 23] = 2;
-        playArea[7, 23] = 3;
-        playArea[6, 23] = 4;
-        playArea[5, 23] = 5;
-        playArea[4, 23] = 6;
-        playArea[3, 23] = 7;
-        playArea[2, 23] = 6;
-        playArea[1, 23] = 5;
-        playArea[0, 23] = 4;
+        return c;
+    }
 
 
+    /// <summary>
+    /// Zoomataan kamera oikealla lailla
+    /// ja laitetaan tausta mustaksi
+    /// </summary>
+    private void SetupLevel()
+    {
         Camera.Zoom(0.5);
         Camera.X = 100;
         Camera.Y = 500;
         Level.BackgroundColor = Color.Black;
-        Timer t = new Timer();
-        t.Interval = 1.0;
+    }
+
+
+    /// <summary>
+    /// Tehään timeri updatelooppia varten
+    /// </summary>
+    private void SetupUpdateLoop()
+    {
+        Timer t = new Timer
+        {
+            Interval = 1.0
+        };
         t.Timeout += UpdateLoop;
         t.Start();
+    }
 
-        PhoneBackButton.Listen(ConfirmExit, "Lopeta peli");
-        Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
+
+    /// <summary>
+    /// Alustetaan taulukot käyttökuntoon
+    /// </summary>
+    private void SetupArrays()
+    {
+        staticArray = new int[drawArray.GetLength(0), drawArray.GetLength(1)];
+        dynamicArray = new int[drawArray.GetLength(0), drawArray.GetLength(1)];
+
+        for (int x = 0; x < drawArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < drawArray.GetLength(1); y++)
+            {
+                drawArray[x, y] = 0;
+                staticArray[x, y] = 0;
+                dynamicArray[x, y] = 0;
+            }
+        }
     }
 
 
@@ -62,24 +128,69 @@ public class Tetris : PhysicsGame
     /// </summary>
     private void UpdateLoop()
     {
-        UpdateTable(playArea);
+        drawArray = CombineArrays(staticArray, dynamicArray);
+
+        for (int x = 0; x < dynamicArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < dynamicArray.GetLength(1); y++)
+            {
+                if (dynamicArray[x, y] != 0)
+                {
+                    if (CanMoveDown(staticArray, x, y))
+                    {
+                        dynamicArray = MoveDown(dynamicArray, x, y);
+                    }
+                    else
+                    {
+                        ReverseMoveUp(x, y);
+                        staticArray = CombineArrays(staticArray, dynamicArray);
+                        EmptyDynamicArray();
+                        /*
+                        staticArray[x, y] = dynamicArray[x, y];
+                        dynamicArray[x, y] = 0;
+                        */
+                    }
+                }
+            }
+        }
+
     }
 
 
     /// <summary>
-    /// Hoidetaan taulukkohommat erillään
+    /// Helpompi siirtää jo siirretyt palikat takas ylöspäin
+    /// kuin ensin katsoa voiko kaikkia palikoita siirtää,
+    /// tallentaa se johonkin ja sitten siirtää ne kaikki
     /// </summary>
-    /// <param name="playArea"></param>
-    public static void UpdateTable(int[,] playArea)
+    /// <param name="cx">missä kohtaa x oltiin menossa</param>
+    /// <param name="cy">missä kohtaa y oltiin menossa</param>
+    private void ReverseMoveUp(int cx, int cy)
     {
-        for (int x = 0; x < playArea.GetLength(0); x++)
+        cx--;
+        for (int x = cx; x >= 0; x--)
         {
-            for (int y = 0; y < playArea.GetLength(1); y++)
+            for (int y = cy; y >= 0; y--)
             {
-                if (CanMoveDown(playArea, x, y))
+                if(dynamicArray[x,y] != 0)
                 {
-                    playArea = MoveDown(playArea, x, y);
+                    dynamicArray[x, y + 1] = dynamicArray[x, y];
+                    dynamicArray[x, y] = 0;
                 }
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Tehdään dynaamisesta taulukosta tyhjä
+    /// </summary>
+    private void EmptyDynamicArray()
+    {
+        for (int x = 0; x < dynamicArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < dynamicArray.GetLength(1); y++)
+            {
+                dynamicArray[x,y] = 0;
             }
         }
     }
@@ -89,15 +200,15 @@ public class Tetris : PhysicsGame
     /// Liikuttaa taulukossa olevaa lukua
     /// y-akselilla alaspäin yhdellä
     /// </summary>
-    /// <param name="playArea"></param>
+    /// <param name="array"></param>
     /// <param name="x"></param>
     /// <param name="y"></param>
     /// <returns></returns>
-    public static int[,] MoveDown(int[,] playArea, int x, int y)
+    public static int[,] MoveDown(int[,] array, int x, int y)
     {
-        playArea[x, y - 1] = playArea[x, y];
-        playArea[x, y] = 0;
-        return playArea;
+        array[x, y - 1] = array[x, y];
+        array[x, y] = 0;
+        return array;
     }
 
 
@@ -105,15 +216,15 @@ public class Tetris : PhysicsGame
     /// Tarkistaa voiko kyseisessä koordinaatissa
     /// olevan jutun siirtää yhtä alemmaksi
     /// </summary>
-    /// <param name="playArea">taulukko</param>
+    /// <param name="array">taulukko</param>
     /// <param name="x">d'oh</param>
     /// <param name="y">d'oh</param>
     /// <returns>voiko vai eikö</returns>
-    public static bool CanMoveDown(int[,] playArea, int x, int y)
+    public static bool CanMoveDown(int[,] array, int x, int y)
     {
-        if(y > 0)
+        if (y > 0)
         {
-            if(playArea[x,y-1] == 0)
+            if (array[x, y - 1] == 0)
             {
                 return true;
             }
@@ -124,28 +235,25 @@ public class Tetris : PhysicsGame
 
     /// <summary>
     /// Piirtää pelikentän
-    /// TODO: Käytä canvas.DrawImage();
-    /// TODO: Piirrä tyhjät ruudut harmaalla yms
     /// </summary>
+    /// TODO: Älä piirrä neljään ylimpään riviin harmaita neliöitä koska jos staticArrayssä on siellä palikoita, peli loppuu
     protected override void Paint(Canvas canvas)
     {
-        for (int x = 0; x < playArea.GetLength(0); x++)
+        int[,] draw = drawArray; //visuaalista debuggausta varten
+
+        for (int x = 0; x < draw.GetLength(0); x++)
         {
-            for (int y = 0; y < playArea.GetLength(1); y++)
+            for (int y = 0; y < draw.GetLength(1); y++)
             {
-                if (playArea[x, y] != 0)
-                {
-                    canvas.BrushColor = NumberToColor(playArea[x, y]);
+                canvas.BrushColor = NumberToColor(draw[x, y]);
 
-                    double tx = x * size;
-                    double ty = y * size;
+                double tx = x * size;
+                double ty = y * size;
 
-                    canvas.DrawLine(tx - dSize, ty - dSize, tx - dSize, ty + dSize);
-                    canvas.DrawLine(tx + dSize, ty + dSize, tx + dSize, ty - dSize);
-
-                    canvas.DrawLine(tx - dSize, ty + dSize, tx + dSize, ty + dSize);
-                    canvas.DrawLine(tx + dSize, ty - dSize, tx - dSize, ty - dSize);
-                }
+                canvas.DrawLine(tx - dSize, ty - dSize, tx - dSize, ty + dSize + 1); //+1 pakollinen ettei jää puuttumaan nurkata pikseliä
+                canvas.DrawLine(tx + dSize, ty + dSize, tx + dSize, ty - dSize);
+                canvas.DrawLine(tx - dSize, ty + dSize, tx + dSize, ty + dSize);
+                canvas.DrawLine(tx + dSize, ty - dSize, tx - dSize, ty - dSize);
             }
         }
         base.Paint(canvas);
@@ -159,9 +267,9 @@ public class Tetris : PhysicsGame
     /// <returns>Numeroa vastaavan värin</returns>
     public static Color NumberToColor(int n)
     {
-        Color[] colors = { Color.Cyan, Color.Yellow, Color.Purple, Color.Green, Color.Blue, Color.Red, Color.Orange };
+        Color[] colors = { Color.DarkGray, Color.Cyan, Color.Yellow, Color.Purple, Color.Green, Color.Blue, Color.Red, Color.Orange };
 
-        return colors[n - 1];
+        return colors[n];
     }
 
 
