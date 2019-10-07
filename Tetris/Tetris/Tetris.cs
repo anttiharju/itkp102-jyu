791c3c2e470141e91a24e49ccdd252f988a788af
anttiharju <antti@harju.io>
2019-10-07 09:22:02 +0000

    Luennolla kerrottiin että pitäisi olla private attribuutteja
    git-svn-id: https://svn.cc.jyu.fi/srv/svn/ohj1-pelit/2019s/anvemaha/trunk@38476 a045da7b-9728-470d-ae4b-0a4eae343f79

--- Tetris/Tetris/Tetris.cs
+++ Tetris/Tetris/Tetris.cs
@@ -7,42 +7,42 @@ using Jypeli.Widgets;
 
 public class Tetris : PhysicsGame
 {
-    public int size = 50;
-    public int dSize = 22;
+    private int size = 50;
+    private int dSize = 22;
 
-    public int forcedShape = 0;
-    public int currentShape = 0;
-    public int upcomingShape = 0;
-    public int heldShape = 0;
-    public int currentRotation = 0;
+    private int forcedShape = 0;
+    private int currentShape = 0;
+    private int upcomingShape = 0;
+    private int heldShape = 0;
+    private int currentRotation = 0;
 
-    public Timer updateTimer = new Timer { Interval = 0.3 };
-    public Timer freefallTimer = new Timer { Interval = 0.03 };
+    private Timer updateTimer = new Timer { Interval = 0.3 };
+    private Timer freefallTimer = new Timer { Interval = 0.03 };
 
-    public int[,] drawArray = new int[10, 24];
-    public int[,] staticArray;
-    public int[,] dynamicArray;
-    public int[,] upcomingArray = new int[4, 4];
-    public int[,] holdArray = new int[4, 4];
+    private int[,] drawArray = new int[10, 24];
+    private int[,] staticArray;
+    private int[,] dynamicArray;
+    private int[,] upcomingArray = new int[4, 4];
+    private int[,] holdArray = new int[4, 4];
 
-    public bool freefall = false;
-    public bool spawn = false;
-    public bool canHold = true;
-    public bool lost = false;
+    private bool freefall = false;
+    private bool spawn = false;
+    private bool canHold = true;
+    private bool lost = false;
 
-    public int[] shapeArraySize = { 4, 2, 3, 3, 3, 3, 3 };
-    public string[] shapeStartPositons = { "220", "421", "420", "321", "421", "421", "321" };
-    public string[] shapes = { "stick", "block", "t", "worm", "corner", "wormR", "cornerR" };
+    private int[] shapeArraySize = { 4, 2, 3, 3, 3, 3, 3 };
+    private string[] shapeStartPositons = { "220", "421", "420", "321", "421", "421", "321" };
+    private string[] shapes = { "stick", "block", "t", "worm", "corner", "wormR", "cornerR" };
 
-    public string[] stick = { "0010001000100010", "0000000011110000" };
-    public string[] block = { "2222" };
-    public string[] t = { "000333030", "030033030", "030333000", "030330030" };
-    public string[] worm = { "044440000", "400440040" };
-    public string[] corner = { "000555005", "055050050", "500555000", "050050550" };
-    public string[] wormR = { "660066000", "060660600" };
-    public string[] cornerR = { "777700000", "700700770", "000007777", "770070070" };
+    private string[] stick = { "0010001000100010", "0000000011110000" };
+    private string[] block = { "2222" };
+    private string[] t = { "000333030", "030033030", "030333000", "030330030" };
+    private string[] worm = { "044440000", "400440040" };
+    private string[] corner = { "000555005", "055050050", "500555000", "050050550" };
+    private string[] wormR = { "660066000", "060660600" };
+    private string[] cornerR = { "777700000", "700700770", "000007777", "770070070" };
 
-    List<string[]> shapeStrings = new List<string[]>();
+    private List<string[]> shapeStrings = new List<string[]>();
 
     public override void Begin()
     {
